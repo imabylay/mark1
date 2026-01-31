b92f561b94db376cccebd111c640354cdcae321a
imabylay <ab_adilbek@kbtu.kz>
2026-01-31 14:40:53 +0500

    data type

---
+++ Practic 2/python-basics/data_type.py
@@ -0,0 +1,20 @@
+# 1
+x = 5
+print(type(x))
+
+
+# 2
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+# 3
+x = bytearray(5)
+print(type(x))
+
+# 4
+x = list(("apple", "banana", "cherry"))
+print(type(x))
+
+# 5
+x = frozenset(("apple", "banana", "cherry"))
+print(type(x))
