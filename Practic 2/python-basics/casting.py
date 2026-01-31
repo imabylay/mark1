9b57f2eaf61af2fe66d6b30b5fc88a5e2694ee82
imabylay <ab_adilbek@kbtu.kz>
2026-01-31 14:48:47 +0500

    casting

---
+++ Practic 2/python-basics/casting.py
@@ -0,0 +1,47 @@
+# 1
+x = int(5)
+y = int(3.9)
+z = int("12")
+
+print(x)  # 5
+print(y)  # 3
+print(z)  # 12
+
+
+
+# 2
+a = float(7)
+b = float(2.5)
+c = float("10")
+d = float("6.75")
+
+print(a)  # 7.0
+print(b)  # 2.5
+print(c)  # 10.0
+print(d)  # 6.75
+
+
+# 3
+x = str("hello")
+y = str(100)
+z = str(3.14)
+
+print(x)
+print(y)
+print(z)
+
+
+
+# 4
+x = int("8")
+y = float(4)
+z = str(9.5)
+
+print(type(x))  # <class 'int'>
+print(type(y))  # <class 'float'>
+print(type(z))  # <class 'str'>
+
+
+# 5
+x = int(float("3.5"))
+print(x)  # 3
