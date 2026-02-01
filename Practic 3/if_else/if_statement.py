1b1c7cc045589285b2216b5a2ac5d2d6ff7a8a8e
imabylay <ab_adilbek@kbtu.kz>
2026-02-01 21:51:17 +0500

    Add basic if statement examples in Python

---
+++ Practic 3/if_else/if_statement.py
@@ -0,0 +1,28 @@
+# Basic if statements
+
+# 1
+a = 33
+b = 200
+
+if b > a:
+    print("b is greater than a")
+
+# 2
+number = 15
+if number > 0:
+    print("The number is positive")
+
+# 3
+age = 20
+if age >= 18:
+    print("You are an adult")
+
+# 4
+is_logged_in = True
+if is_logged_in:
+    print("Welcome back!")
+
+# 5
+value = -3
+if value:
+    print("Value is treated as True")
