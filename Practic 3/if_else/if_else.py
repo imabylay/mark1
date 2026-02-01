3c218a5d4984b7e04d3425b7db998aebf8ab68fd
imabylay <ab_adilbek@kbtu.kz>
2026-02-01 21:53:01 +0500

    Add if-else control flow examples

---
+++ Practic 3/if_else/if_else.py
@@ -0,0 +1,38 @@
+# If-else statements
+
+# 1
+a = 200
+b = 33
+
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
+
+# 2
+number = 7
+if number % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
+
+# 3
+username = ""
+if username:
+    print("Welcome,", username)
+else:
+    print("Username is empty")
+
+# 4
+score = 85
+if score >= 60:
+    print("Passed")
+else:
+    print("Failed")
+
+# 5
+is_active = False
+if is_active:
+    print("Account is active")
+else:
+    print("Account is inactive")
