935c9d80f6fb37057953b93820ef8e7f312b4311
imabylay <ab_adilbek@kbtu.kz>
2026-02-01 22:10:21 +0500

    Add basic for loop examples

---
+++ Practic 3/loops/for_loop.py
@@ -0,0 +1,22 @@
+# Basic for loop examples
+
+#  1
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+#  2
+for char in "banana":
+    print(char)
+
+#  3
+for i in range(5):
+    print(i)
+
+#  4
+for i in range(2, 6):
+    print(i)
+
+#  5
+for i in range(2, 20, 3):
+    print(i)
