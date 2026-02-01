4740471e24f96b0ce00297c89a2c299851df8f45
imabylay <ab_adilbek@kbtu.kz>
2026-02-01 21:41:35 +0500

    and, or, not

---
+++ Practic 3/boolean/boolean_operators.py
@@ -0,0 +1,23 @@
+# Python logical operators
+
+x = 5
+
+# 1: AND operator
+print(x > 0 and x < 10)
+
+
+# 2: AND returning False
+print(x > 10 and x < 20)
+
+
+
+# 3: OR operator
+print(x < 5 or x > 3)
+
+
+# 4: OR returning False
+print(x < 3 or x > 10)
+
+
+# 5: NOT operator
+print(not(x > 3 and x < 10))
