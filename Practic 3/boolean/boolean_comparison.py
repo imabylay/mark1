bd41d256c3299116b2613cdc3956680cdd4f3691
imabylay <ab_adilbek@kbtu.kz>
2026-02-01 21:38:46 +0500

    ==, !=, <, >, <=, >=

---
+++ Practic 3/boolean/boolean_comparison.py
@@ -0,0 +1,37 @@
+# Python comparison operators
+
+x = 5
+y = 3
+
+# 1: Equal and not equal
+print(x == y)
+print(x != y)
+
+print("-" * 30)
+
+# 2: Greater than and less than
+print(x > y)
+print(x < y)
+
+print("-" * 30)
+
+# 3: Greater than or equal / less than or equal
+print(x >= y)
+print(x <= y)
+
+print("-" * 30)
+
+# 4: Comparison with different values
+a = 10
+b = 10
+
+print(a == b)
+print(a > b)
+
+print("-" * 30)
+
+# 5: Chained comparisons
+z = 7
+
+print(1 < z < 10)
+print(5 < z < 6)
